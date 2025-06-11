9f548c5253b97c2cc5705aae772941271b99e64f
razeemarc <razeemarc@gmail.com>
2025-06-11 05:34:30 +0530

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -38,12 +38,15 @@ export function Navbar() {
   }
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-100 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2 md:gap-6">
-          <Link href="/" className="font-bold text-xl ml-10">
-           Election management 
-          </Link>
+        {user?.role !== "ADMIN" && (
+  <Link href="/" className="font-bold text-xl ml-10">
+    Election management 
+  </Link>
+)}
+
           <nav className="hidden md:flex items-center gap-6">
            
           </nav>
